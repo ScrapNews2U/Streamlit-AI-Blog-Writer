36aa02824395e3b22a0b1a434df3cb2587645b64
ScrapNews2U <ScrapNews2U@gmail.com>
2023-11-21 12:53:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+# pip install streamlit
+# 실행 : streamlit run [사용자지정 이름].py
+
 import streamlit as st
 
 st.set_page_config(
     page_title="AI Blog Writer"
 )
 
-st.title("hello world")
-st.title("hello world")
 st.title("hello world")
